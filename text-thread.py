7ca3c02c4079c696f634b9459b322bdabedd8f19
Sean Rose <sean.andrew.rose@gmail.com>
2012-04-19 20:09:48 -0700

    added placeholders for box/twilio endpoints

--- text-thread.py
+++ text-thread.py
@@ -7,6 +7,17 @@ app = Flask(__name__)
 def hello():
     return 'Hello World!'
 
+#Endpoint that handles Twilio callbacks
+@app.route('/twilio', , methods=['GET', 'POST'])
+def convert_text_to_comment():
+	return 'Hello Twilio!'
+
+#Endpoint that handles Box authentication
+@app.route('/box', methods=['GET', 'POST'])
+def box():
+	return 'Hello Box!'
+    
+
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
