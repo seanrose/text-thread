2cac7284d4ea65548615bc4d707a4f195d9b5580
Sean Rose <sean.andrew.rose@gmail.com>
2012-04-25 02:08:08 -0400

    Adding Twilio message handling, Box comment uploading

--- text-thread.py
+++ text-thread.py
@@ -1,25 +1,37 @@
 import os
+import urllib2
 
 Debug = True
 
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
 
-#Endpoint that handles Twilio callbacks
-@app.route('/twilio', methods=['GET', 'POST'])
+@app.route('/twilio', methods=['GET'])
 def convert_text_to_comment():
-	return 'Hello Twilio!'
+	"""
+	Twilio will post any texts it receives
+	to this endpoint. We will do the following:
 
-#Endpoint that handles Box authentication
-@app.route('/box', methods=['GET', 'POST'])
-def box():
-	return 'Hello Box!'
+	-Get the text message from Twilio
+	-Convert it into a Box comment object
+	-Send the comment to Box 
+	"""
+
+	#Get the text message sent by Twilio
+	message = request.args.get('Body')
+
+	#Set up the comment to send to box
+	box_comment = "{'message':'%s'}", message
+	api_url = "https://www.box.com/api/2.0/files/%s", os.environ['BOX_FILE']
+	api_header = "Authorization : BoxAuth api_key=%s&auth_token=%s", 
+		os.environ['BOX_API_KEY'],os.environ['BOX_AUTH_TOKEN']
+
+	#Build the request
+	req = urllib2.Request(api_url, box_comment, api_header)
+	response = urllib2.urlopen(req)
+	print response
     
-
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
